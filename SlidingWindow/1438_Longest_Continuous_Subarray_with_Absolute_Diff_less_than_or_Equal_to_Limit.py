611409b6db07a3a31688666b27642e3c2e5c0aed
Peixian Liang <pliang@crcfe01.crc.nd.edu>
2020-09-19 19:42:45 -0400

    1438

---
+++ SlidingWindow/1438_Longest_Continuous_Subarray_with_Absolute_Diff_less_than_or_Equal_to_Limit.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+
+        min_stack = deque()
+        max_stack = deque()
+        left = 0
+        indicate = 0
+        result = 0
+        for right, n in enumerate(nums):
+            while min_stack and n <= nums[min_stack[-1]]:
+                min_stack.pop()
+            if not min_stack:
+                indicate = 0
+            min_stack.append(right)
+
+            while max_stack and n >= nums[max_stack[-1]]:
+                max_stack.pop()
+            if not max_stack:
+                indicate = 1
+            max_stack.append(right)
+
+            if nums[max_stack[0]] - nums[min_stack[0]] > limit:
+                result = max(result, right - left)
+                if indicate:
+                    while nums[max_stack[0]] - nums[min_stack[0]] > limit:
+                        left = min_stack.popleft() + 1
+                else:
+                    while nums[max_stack[0]] - nums[min_stack[0]] > limit:
+                        left = max_stack.popleft() + 1
+
+        if nums[max_stack[0]] - nums[min_stack[0]] <= limit:
+            result = max(result, right - left + 1)
+        return result
